5bb50f39750cffaf8ed60149c48d9449f922327d
Devdat Kumar <Devdat_Kumar@Outlook.com>
2024-11-16 08:36:42 +0500

    example schema auth-realated tables.

---
+++ src/db/schema.ts
@@ -0,0 +1,5 @@
+export { accounts } from "./schema/accounts";
+export { authenticators } from "./schema/authenticators";
+export { sessions } from "./schema/sessions";
+export { users } from "./schema/users";
+export { verificationTokens } from "./schema/verificationtokens";
